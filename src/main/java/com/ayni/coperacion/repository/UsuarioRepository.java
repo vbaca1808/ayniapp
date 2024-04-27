bae5253694789394eba32070c5e33560eeaf70bf
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-04-27 11:54:44 -0500

    ultimos ajsutes

--- src/main/java/com/ayni/coperacion/repository/UsuarioRepository.java
+++ src/main/java/com/ayni/coperacion/repository/UsuarioRepository.java
@@ -173,14 +173,15 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
                                       @Param("diaSeleccionado") int diaSeleccionado);
 
     @Query( value = "call sp_actualizar_negocio_pedido(:idNegocio, :idProducto, " +
-	":nombreProducto, :precio, :estado, :stockInicial, :codigoBarra)", nativeQuery = true)
+	":nombreProducto, :precio, :estado, :stockInicial, :codigoBarra, :recetaInsumo)", nativeQuery = true)
     List<RespuestaStd> actualizarNegocioPedido(@Param("idNegocio") int idNegocio,  
                                                @Param("idProducto") int idProducto,
                                                @Param("nombreProducto") String nombreProducto,
                                                @Param("precio") BigDecimal precio,
                                                @Param("estado") int estado,
                                                @Param("stockInicial") BigDecimal stockInicial,
-                                               @Param("codigoBarra") String codigoBarra);
+                                               @Param("codigoBarra") String codigoBarra,
+                                               @Param("recetaInsumo") String recetaInsumo);
 
     @Query( value = "call sp_agregarquita_admin_usuario(:idUsuario, :nombreUsuario, :isAdmin)", nativeQuery = true)
     List<RespuestaStd> agregarquitaAdminUsuario(@Param("idUsuario") int idUsuario,  
