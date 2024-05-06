870139efded70f6c69a48dcafa8f5adcb3a07fd0
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-05-06 14:01:44 -0500

    ultimos ajsutes

--- src/main/java/com/ayni/coperacion/controller/CooperacionController.java
+++ src/main/java/com/ayni/coperacion/controller/CooperacionController.java
@@ -559,6 +559,7 @@ public class CooperacionController {
     @PostMapping(value="/actualizarlecturacocina/{idnegocio}/{idpedido}/{idproducto}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<RespuestaStd>> actualizarLecturaCocina(
         @PathVariable int idnegocio,@PathVariable int idpedido,@PathVariable int idproducto) {
+        
         try { 
             List<RespuestaStd> lst = iUsuarioService.actualizarLecturaCocina(idnegocio, idpedido, idproducto);
             return ResponseEntity.ok().body(lst);
