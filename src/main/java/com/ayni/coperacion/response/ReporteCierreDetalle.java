41e27944208c0ead26ec5eb59dbb9f180e06c837
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-05-08 16:45:59 -0500

    ultimos ajsutes

--- src/main/java/com/ayni/coperacion/response/ReporteCierreDetalle.java
+++ src/main/java/com/ayni/coperacion/response/ReporteCierreDetalle.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 public interface ReporteCierreDetalle {
     
     String getDescripcionProducto();  
+    String getObsMesa();  
+    String getObsLLevar();  
 	BigDecimal getPrecio();
     BigDecimal getCantidad(); 
     BigDecimal getCantidadAtendidaParaLlevar(); 
