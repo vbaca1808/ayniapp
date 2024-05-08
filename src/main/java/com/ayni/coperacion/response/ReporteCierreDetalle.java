eabc615b6daaf6d00f37660598f5d198f4f4993c
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-05-08 12:09:24 -0500

    ultimos ajsutes

--- src/main/java/com/ayni/coperacion/response/ReporteCierreDetalle.java
+++ src/main/java/com/ayni/coperacion/response/ReporteCierreDetalle.java
@@ -3,11 +3,11 @@ package com.ayni.coperacion.response;
 import java.math.BigDecimal;
 
 public interface ReporteCierreDetalle {
-    int getIdPedido(); 
-    int getMesa();
+    
     String getDescripcionProducto();  
 	BigDecimal getPrecio();
     BigDecimal getCantidad(); 
     BigDecimal getCantidadAtendidaParaLlevar(); 
 	BigDecimal getTotal();
+
 }
