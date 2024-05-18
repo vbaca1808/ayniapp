926fc35bbf51276a8dc74673d8b68258b1f45282
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-05-18 11:47:57 -0500

    ajuste

--- src/main/java/com/ayni/coperacion/response/ConfiguracionNegocio.java
+++ src/main/java/com/ayni/coperacion/response/ConfiguracionNegocio.java
@@ -10,4 +10,6 @@ public interface ConfiguracionNegocio {
     int getRubroNegocio(); 
     int getUsarLectorBarraBusquedaManual(); 
     int getEnvioPlatoDirectoACocina();
+    int getCorreoElectronico();
+    
 }
