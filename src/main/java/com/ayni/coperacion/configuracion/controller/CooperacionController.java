1ca888d0ac654e66f8f961c0bd49a1135b1bf3a6
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-05-18 14:59:16 -0500

    ajuste

--- src/main/java/com/ayni/coperacion/configuracion/controller/CooperacionController.java
+++ src/main/java/com/ayni/coperacion/configuracion/controller/CooperacionController.java
@@ -821,7 +821,7 @@ public class CooperacionController {
                             dataRow.createCell(0).setCellValue(lReporteCierre.get(i).getDato2());
                             dataRow.createCell(1).setCellValue(lReporteCierre.get(i).getDato3());
                             dataRow.createCell(2).setCellValue(lReporteCierre.get(i).getDato6());
-                            dataRow.createCell(3).setCellValue(lReporteCierre.get(i).getDato5());
+                            dataRow.createCell(3).setCellValue(lReporteCierre.get(i).getDato6());
                         } else if (idrubronegocio == 2) {
                             dataRow.createCell(0).setCellValue(lReporteCierre.get(i).getDato1() + " " + lReporteCierre.get(i).getDato2());
                             dataRow.createCell(1).setCellValue(lReporteCierre.get(i).getDato4());
@@ -835,9 +835,7 @@ public class CooperacionController {
                         Row dataRow = sheetTp.createRow(filaTp);
                         if (idrubronegocio == 1) {
                             dataRow.createCell(0).setCellValue(lReporteCierre.get(i).getDato2());
-                            dataRow.createCell(1).setCellValue(lReporteCierre.get(i).getDato3());
-                            dataRow.createCell(2).setCellValue(lReporteCierre.get(i).getDato6());
-                            dataRow.createCell(3).setCellValue(lReporteCierre.get(i).getDato5());
+                            dataRow.createCell(1).setCellValue(lReporteCierre.get(i).getDato1()); 
                         } else if (idrubronegocio == 2) {
                             dataRow.createCell(0).setCellValue(lReporteCierre.get(i).getDato2());
                             dataRow.createCell(1).setCellValue(lReporteCierre.get(i).getDato1()); 
@@ -848,9 +846,9 @@ public class CooperacionController {
                         filaPd++;
                         Row dataRow = sheetPd.createRow(filaPd);
                         if (idrubronegocio == 1) {
-                            dataRow.createCell(0).setCellValue(lReporteCierre.get(i).getDato2());
+                            dataRow.createCell(0).setCellValue(lReporteCierre.get(i).getDato1() + lReporteCierre.get(i).getDato2());
                             dataRow.createCell(1).setCellValue(lReporteCierre.get(i).getDato3());
-                            dataRow.createCell(2).setCellValue(lReporteCierre.get(i).getDato6());
+                            dataRow.createCell(2).setCellValue(lReporteCierre.get(i).getDato4());
                             dataRow.createCell(3).setCellValue(lReporteCierre.get(i).getDato5());
                         } else if (idrubronegocio == 2) {
                             dataRow.createCell(0).setCellValue(lReporteCierre.get(i).getDato1() + "" + lReporteCierre.get(i).getDato2());
