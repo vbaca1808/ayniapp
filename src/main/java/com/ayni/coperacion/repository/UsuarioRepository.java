8786a2f90ed80f1f9c89e5debb23125e50641d99
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-05-18 11:51:04 -0500

    ajuste

--- src/main/java/com/ayni/coperacion/repository/UsuarioRepository.java
+++ src/main/java/com/ayni/coperacion/repository/UsuarioRepository.java
@@ -264,7 +264,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
                                                @Param("nombreUsuario") String nombreUsuario);
     
     @Query( value = "call sp_configuracion_negocio(:idNegocio, :nombreNegocio, :descripcion, " +  
-     ":logo, :estadoNegocio, :rubroNegocio, :usarLectorBarraBusquedaManual, :envioPlatoDirectoACocina)", nativeQuery = true)
+     ":logo, :estadoNegocio, :rubroNegocio, :usarLectorBarraBusquedaManual, :envioPlatoDirectoACocina, :correoElectronico)", nativeQuery = true)
     List<RespuestaStd> configuracionNegocio(@Param("idNegocio") int idNegocio,  
                                             @Param("nombreNegocio") String nombreNegocio,  
                                             @Param("descripcion") String descripcion,  
