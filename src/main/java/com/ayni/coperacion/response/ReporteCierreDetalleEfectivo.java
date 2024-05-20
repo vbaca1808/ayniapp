effd1a61fde2b2e539191282c1f4fdbd47f8ccf4
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-05-20 12:54:58 -0500

    ajuste

--- src/main/java/com/ayni/coperacion/response/ReporteCierreDetalleEfectivo.java
+++ src/main/java/com/ayni/coperacion/response/ReporteCierreDetalleEfectivo.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 
 public interface ReporteCierreDetalleEfectivo {
     
-    int getMesa();  
+    String getMesa();  
     int getIdPedido();  
     String getFechaPago(); 
     BigDecimal getMontoPago();
