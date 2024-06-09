0dd92ab0a8bf7a20ed94f62efa7deb5e85545160
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-06-09 15:38:05 -0500

    ajuste

---
+++ src/main/java/com/ayni/coperacion/service/MyScheduledTasks.java
@@ -0,0 +1,14 @@
+package com.ayni.coperacion.service;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyScheduledTasks {
+ 
+    @Scheduled(cron = "0 0/5 * * * *") // Se ejecuta cada hora
+    public void sendHourlyEmail() {
+        // Coloca aquí el código para enviar el correo
+    }
+
+}
