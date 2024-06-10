000c85d78da56e332d3c865abe64e4631bd6f9a5
ayniapp24@gmail.com <ayniapp24@gmail.com>
2024-06-10 15:06:44 -0500

    ajuste

--- src/main/java/com/ayni/coperacion/_implementation/UsuarioServiceImpl.java
+++ src/main/java/com/ayni/coperacion/_implementation/UsuarioServiceImpl.java
@@ -751,8 +751,8 @@ public class UsuarioServiceImpl implements IUsuarioService {
         try {
             return usuarioRepository.modificarMenuPedidoUnitario(menuPedidoUnitario.getIdNegocio(), 
             menuPedidoUnitario.getIdPedido(), menuPedidoUnitario.getIdProducto(), 
-            menuPedidoUnitario.getCantidad(), menuPedidoUnitario.getCantidadLLevar(),menuPedidoUnitario.getTaper().intValue(),
-            menuPedidoUnitario.getDescripcion(), menuPedidoUnitario.getTotal(), new Date());
+            menuPedidoUnitario.getCantidad(), menuPedidoUnitario.getCantidadLLevar(), menuPedidoUnitario.getTaper().intValue(),
+            menuPedidoUnitario.getDescripcion().replace("''", "'"), menuPedidoUnitario.getTotal(), new Date());
 
         } catch (Exception e) {
             throw new UnsupportedOperationException("Unimplemented method 'modificarMenuPedidoUnitario'");
